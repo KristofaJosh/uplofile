dd422b591e8bfb4ec971c89b890967fb8446d02b
kristofajosh <christopherjoshua18@live.com>
2025-09-12 01:12:22 +0100

    prepare docs

--- app/webdoc/app/layout.tsx
+++ app/webdoc/app/layout.tsx
@@ -1,22 +1,49 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { Metadata } from "next";
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Uplofile Webdoc",
   description: "Demo/Docs for the uplofile component library",
 };
 
-export default function RootLayout({
+const banner = <Banner storageKey="some-key">Uplofile is in beta</Banner>;
+const navbar = <Navbar logo={<b>Uplofile</b>} />;
+const footer = (
+  <Footer>
+    By{" "}
+    <b>
+      <a href="#">Kristofajosh</a>
+    </b>
+  </Footer>
+);
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-gray-50 text-gray-900">
-        <div className="mx-auto max-w-3xl p-6">{children}</div>
+    <html lang="en" dir="ltr" suppressHydrationWarning>
+      <Head>
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          footer={footer}
+        >
+          {children}
+        </Layout>
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   );
 }
--- app/webdoc/next.config.mjs
+++ app/webdoc/next.config.mjs
@@ -1,5 +1,10 @@
-const nextConfig = {
-  reactStrictMode: true,
-};
+import nextra from "nextra";
 
-export default nextConfig;
+const withNextra = nextra({
+  // Add Nextra-specific options here
+});
+
+export default withNextra({
+  // Add regular Next.js options here
+  reactStrictMode: true,
+});
