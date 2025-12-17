519c804c9e810528d3f277bda68d781492a88729
kristofajosh <christopherjoshua18@live.com>
2025-12-17 13:13:25 +0100

    feat: setup React-based playground with file upload functionality and testing setup

---
+++ app/playground/src/App.js
@@ -0,0 +1,120 @@
+import "./App.css";
+import {
+  UplofileCancel,
+  UplofileDropzone,
+  UplofilePreview,
+  UplofileRemove,
+  UplofileRetry,
+  UplofileRoot,
+  UplofileTrigger,
+} from "./components/ui/uplofile";
+import { clsx } from "clsx";
+
+const upload = async (file, signal, setProgress) => {
+  return await new Promise((resolve, reject) => {
+    let progress = 0;
+    const interval = setInterval(
+      () => {
+        const increment = Math.floor(Math.random() * 16) + 5; // 5 - 20
+        progress += increment;
+        setProgress(progress > 100 ? 100 : progress);
+        if (progress >= 100) {
+          clearInterval(interval);
+          resolve({ url: URL.createObjectURL(file) });
+        }
+      },
+      Math.floor(Math.random() * 701) + 300,
+    ); // 300 - 1000 ms
+  });
+};
+
+const onRemove = async (item) => {
+  return await new Promise((resolve, reject) =>
+    setTimeout(() => resolve("something went wrong"), 4000),
+  );
+};
+
+function App() {
+  return (
+    <div className="p-10">
+      <UplofileRoot onRemove={onRemove} upload={upload} removeMode={"strict"}>
+        <UplofileDropzone className={"border p-2 rounded"}>
+          <span> Drop your files here</span>{" "}
+          <UplofileTrigger className={"underline text-blue-500"}>
+            Select file
+          </UplofileTrigger>
+          <div className={"border-t my-6 py-6"}>
+            <UplofilePreview />
+          </div>
+        </UplofileDropzone>
+      </UplofileRoot>
+
+      <div className={"mt-10"}>
+        <UplofileRoot onRemove={onRemove} upload={upload}>
+          <div className={"grid grid-cols-[auto_1fr] items-center gap-4"}>
+            <UplofileTrigger asChild className={"p-3 rounded-lg border"}>
+              <button>Pick Files</button>
+            </UplofileTrigger>
+            <UplofilePreview
+              render={({ items, actions }) => {
+                return (
+                  <div className={"flex gap-2"}>
+                    {items.map((item) => {
+                      return (
+                        <div
+                          key={item.uid}
+                          className={"size-14 shadow relative"}
+                        >
+                          {["error", "uploading"].includes(item.status) && (
+                            <UplofileRetry
+                              className={clsx(
+                                "flex items-center justify-center",
+                                "absolute rounded w-full h-full z-10 inset-0 bg-black/60  text-white text-xs",
+                              )}
+                              uid={item.uid}
+                            >
+                              {item.status === "error" ? "Retry" : <span className={'animate-spin font-semibold'}>O</span>}
+                            </UplofileRetry>
+                          )}
+                          <img
+                            className={"object-cover rounded w-full h-full"}
+                            src={item.url || item.previewUrl}
+                            alt={item.name}
+                            key={item.uid}
+                          />
+                          <UplofileCancel
+                            className={clsx(
+                              "bg-white cursor-pointer border rounded-full size-4 m-1text-black text-xs absolute top-1 right-1",
+                              item.status === "uploading" && "z-20",
+                            )}
+                            asChild
+                            uid={item.uid}
+                          >
+                            <button>X</button>
+                          </UplofileCancel>
+                          <UplofileRemove
+                            className={clsx(
+                              "cursor-pointer text-black text-xs",
+                              item.status === "removing" && "opacity-50",
+                              item.status === "error" && "text-red-600",
+                              item.status === "uploading" && "hidden",
+                            )}
+                            uid={item.uid}
+                          >
+                            Remove
+                          </UplofileRemove>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            />
+          </div>
+        </UplofileRoot>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/playground/src/components/ui/uplofile/index.js
@@ -0,0 +1,28 @@
+import * as Uplofile from "uplofile";
+
+const UplofileRoot = Uplofile.Root;
+
+const UplofileTrigger = Uplofile.Trigger;
+
+const UplofileHiddenInput = Uplofile.HiddenInput;
+
+const UplofileDropzone = Uplofile.Dropzone;
+
+const UplofilePreview = Uplofile.Preview;
+
+const UplofileCancel = Uplofile.Cancel;
+
+const UplofileRetry = Uplofile.Retry;
+
+const UplofileRemove = Uplofile.Remove;
+
+export {
+  UplofileRoot,
+  UplofileTrigger,
+  UplofileHiddenInput,
+  UplofileDropzone,
+  UplofilePreview,
+  UplofileCancel,
+  UplofileRetry,
+  UplofileRemove,
+};
